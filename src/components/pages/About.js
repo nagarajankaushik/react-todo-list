665390312fbc685aa571103ecd3496f4d133e2f0
Kaushik Nagarajan <nagarajankaushik@github.com>
2020-06-06 18:25:34 +0530

    explaining the lag

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,9 @@ export default function About() {
     return (
             <div style = {{padding:'10px'}}>
                 <h1>About</h1>
-                <p>This is Kaushik's TodoList app v1.0.0. 
-                    Coded through traversyMedia's online ReactJS crash course</p>
+                <p>This is Kaushik's TodoList app v1.1.2. 
+                    Coded through traversyMedia's online ReactJS crash course.
+                    Please excuse the lag in the app. It is being hosted for free in Netlify!</p>
             </div>
     )
 }
