e51918272b6c9700371c503b589d8047fcc40fdb
Kaushik Nagarajan <nagarajankaushik@github.com>
2020-07-13 09:42:43 +0530

    build folder commit for gh pages

---
+++ build/precache-manifest.6c144e753cb5e2fd818eac9e55527b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d85b6faac5edb05b470d2f706b171ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12cc407f0604daf5f068",
+    "url": "/static/css/main.a99f7520.chunk.css"
+  },
+  {
+    "revision": "57d759c122d879328582",
+    "url": "/static/js/2.b3f61014.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3f61014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cc407f0604daf5f068",
+    "url": "/static/js/main.edf684ff.chunk.js"
+  },
+  {
+    "revision": "5a91f993b94fdef25c6c",
+    "url": "/static/js/runtime-main.de7b4335.js"
+  }
+]);
